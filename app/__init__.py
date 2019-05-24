bc3706aa48c7a8d484901a4479a56eec09df214b
vinay.jadhav <vinay.jadhav@nitorinfotech.com>
2019-05-24 17:47:35 +0530

    Added folders.initialized flask_sqlalchemy,flask_marshmallow

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, Blueprint
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 
 def create_app():
-    app = Flask(__name__)
 
+    app = Flask(__name__)
+    init_packages(app)
+
+
+def init_packages(app):
+    sql_alchemy = SQLAlchemy()
+    marshmallow = Marshmallow()
+    sql_alchemy.init_app(app)
+    marshmallow.init_app(app)
